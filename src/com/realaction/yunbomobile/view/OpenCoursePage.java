83393c2f088b4f8d6b629905f892049041c3f887
liumeng1201 <liumeng1201@gmail.com>
2014-01-20 16:58:13 +0800

    公开课添加跳转到案例列表界面操作

--- src/com/realaction/yunbomobile/view/OpenCoursePage.java
+++ src/com/realaction/yunbomobile/view/OpenCoursePage.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 import com.realaction.yunbomobile.R;
@@ -51,6 +54,14 @@ public class OpenCoursePage extends Fragment {
 		View view = inflater.inflate(R.layout.fragment_opencourse, null);
 		opencourse_list = (ListView) view.findViewById(R.id.opencourse_list);
 		opencourse_list.setAdapter(adapter);
+		opencourse_list.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long location) {
+				Intent intent = new Intent(context, CaseListActivity.class);
+				startActivity(intent);
+			}
+		});
 		return view;
 	}
 
