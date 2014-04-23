5ff3337253523d47b9e3bb7a88907e530f9c223d
liumeng <liumeng1201@gmail.com>
2014-04-23 17:07:49 +0800

    代码为调。

--- src/com/realaction/yunbomobile/utils/AppInfo.java
+++ src/com/realaction/yunbomobile/utils/AppInfo.java
@@ -1,5 +1,7 @@
 package com.realaction.yunbomobile.utils;
 
+import android.os.Environment;
+
 public class AppInfo {
 	// 服务器端基础url
 //	public static final String base_url = "http://192.168.2.231:8080";
@@ -10,5 +12,6 @@ public class AppInfo {
 	// 网络是否可用标志
 	public static boolean network_avabile = true;
 	// 软件基础目录
-	public static final String base_dir = "/mnt/sdcard/.yunbo";
+//	public static final String base_dir = "/mnt/sdcard/.yunbo";
+	public static final String base_dir = Environment.getExternalStorageDirectory().toString() + "/" + ".yunbo";
 }
